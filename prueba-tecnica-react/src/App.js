413b0a1a4b5d095a7a9dbbdfc2bf9b20365b9332
lisandroacba <lacordoba@cordoba.gov.ar>
2025-04-22 16:08:32 -0300

    merge a master

--- prueba-tecnica-react/src/App.js
+++ prueba-tecnica-react/src/App.js
@@ -1,15 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [productos, setProductos] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+
+  const consumoData = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get('https://fakestoreapi.com/products')
+      setProductos(res.data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  if(loading) return <p>Cargando...</p>
+
   return (
     <div className="App">
 
       {/* Aca dibujar el botón */}
+      <div className="contenedorBtn">
+        <button onClick={()=> consumoData()} className='btnApp'>Enviar</button>
+      </div>
 
       {/* Utilizar el div para dibujar todo el listado de productos */}
-      <div className="card">
-      </div>
+      {
+        productos.length ? productos.map(producto => (
+          <>
+            <h3>{producto.title}</h3>
+            <div className="card">
+              <img src={producto.image} alt={producto.title}/>
+            </div>
+            <p>{producto.description}</p>
+            <p>{producto.price}</p>
+          </>
+        )) : <p>No hay productos</p>
+      }
+      
 
     </div>
   );
